8df19877d4d5a8adc8781cd8745114e682a50629
PrinceCarandang <157162387+PrinceCarandang@users.noreply.github.com>
2024-03-10 22:20:39 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prince Keith Carandang - Personal Website</title>
+    <link rel="stylesheet" href="testinglang.css">
+</head>
+
+<body>
+
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="social.php">SOCIALS</a></li>
+                <li><a href="about.php">ABOUT</a></li>
+                <li><a href="feedback.php">FEEDBACK</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+<head>
+    <meta charset="utf-8">
+    <script src="https://use.fontawesome.com/d1341f9b7a.js"></script>
+    <link rel="stylesheet" href="testinglang.css">
+  </head>
+  <body>
+<div class="box">
+  <img src="mehehe.jpg" alt="" class="box-img">
+  <h1>
+Prince Keith Carandang
+</h1>
+<h5>IT Student</h5>
+<p>Second-year IT explorer, fueled by caffeine and curiosity. I unwrap programming puzzles like birthday presents, always eager for the next tech treasure. Whether building bots or battling bugs, I'm on a coding odyssey – and trust me, the adventure's just begun! </p>
+    </div>
+</body>
+
+<footer>
+        <p>&copy; 2024 Prince Max Wynn. All rights reserved.</p>
+    </footer>
+
+</html>
